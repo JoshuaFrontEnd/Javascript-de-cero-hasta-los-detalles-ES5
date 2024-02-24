edc233adbad456728d60ce93435ac26c4cefb1db
Joshua <hola@joshua.cl>
2024-02-24 15:18:39 -0300

    La palabra reservada NEW

--- app.js
+++ app.js
@@ -146,15 +146,15 @@
 // - Si la funcion es llamada sin los parentesis retornara la definicion de la funcion, osea el codigo literal:
 // primeraFuncion
 
-// - Funcion declarativa:
-function sumar( a, b) {
-  return a + b
-}
+// // - Funcion declarativa:
+// function sumar( a, b) {
+//   return a + b
+// }
 
-// - Expresion de funcion:
-var sumar = function ( a, b ) {
-  return a + b
-}
+// // - Expresion de funcion:
+// var sumar = function ( a, b ) {
+//   return a + b
+// }
 
 /*-- ----------------------------------------------------- --*/
 /*--      Argumentos o parametros de las Funciones         --*/
@@ -429,3 +429,216 @@ var sumar = function ( a, b ) {
 // }
 
 // persona.direccion.obtenerPais()
+
+/*-- ----------------------------------------------------- --*/
+/*--                 Palabra reservada NEW                 --*/
+/*-- ----------------------------------------------------- --*/
+
+// - El origen de la palabra reservada "NEW" tiene su origen en que cuando se creo Javascript el creador queria que otros desarrolladores que no conocian tanto este nuevo lenguaje se sintieran familiarizados con la sintaxis de otros lenguajes que si conocian como Java, para que pudieran probarlo y darle una oportunidad
+
+// - En javascript podemos declarar un objeto vacio de la siguiente forma:
+
+// var Tulio = {}
+
+// - Pero tambien podemos hacerlo con la palabra reservada "NEW", al hacerlo de esta manera creamos un objeto/funcion de nombre "Tulio", del tipo "Persona", esto basicamente viene a ser una "funcion constructora", es decir, una funcion que crea objetos
+
+// function Persona(){}
+
+// var Tulio = new Persona()
+
+// console.log( Tulio )
+
+// - Por otro lado al usar "NEW" estamos creando un nuevo "scope" por lo cual podemos usar "THIS" con el valor del "scope" creado:
+
+function Persona( nombre, apellido ){
+
+  this.nombre = nombre;
+  this.apellido = apellido;
+  this.edad = 30;
+
+  this.imprimirPersona = function(){
+    return this.nombre + " " + this.apellido + "(" + this.edad + ")"
+  }
+
+}
+
+var Tulio = new Persona( "Tulio", "Triviño" )
+
+console.log( Tulio )
+
+/*
+
+  Apuntes
+
+  - Comentario hecho por Orlando en: https://www.udemy.com/course/javascript-de-cero-hasta-los-detalles/learn/lecture/4504258#questions/5934670
+
+  ¿Por que las funciones en JavaScript, aparte de poder definir un bloque reusable de codigo, permiten crear objetos?
+
+  En ocasiones necesitaremos encapsular información y comportamiento en entidades individuales llamadas objetos. De esta manera podemos manipular de una mejor manera la logica de nuestros programas.  Asi entonces un objeto consta de una unidad que posee un estado(propiedades) y un comportamiento(métodos); es una colección de propiedades y funcionalidades.
+
+  Ahora en el caso de usar this, tienes que tener en cuenta una cosa.
+
+  Las funciones en javascript se comportan de dos maneras: como funciones simples o constructores.
+
+  Una funcion simple es como ya la conoces :
+
+  function saluda() {
+    console.log("hola");
+  }
+  Una funcion como constructor se usa cuando queramos crear objetos. Como comente antes, un objeto es una colección de propiedades y funcionalidades— conocidos tambien como métodos.
+
+  Una propiedad, como su nombre lo indica, es una caracteristica de un objeto. Un método es una funcion o proceso asociado a un objeto(funcion).
+
+  Ahora, para que un objeto pueda ser creado necesitamos una clase. Una clase se puede pensar como una plantilla que permite crear objetos similares(con propiedades y métodos iguales )pero con información distinta.
+
+  Por ejemplo esta plantilla:
+
+  plantilla persona :
+  ----- edad
+  -----peso
+  -----nombre
+  Una vez tienes tu plantilla entonces puedes crear el objeto. En pocas palabras, un objeto se crea a partir de una clase, quien es la que define las propiedades y métodos que el objeto debe tener.
+
+  Entonces podemos crear nuestro objeto:
+
+  crear objeto Carlos de la plantilla Persona con edad : 17, peso : 42, nombre : "carlos".
+
+  Como veras he llenado las propiedades al crear un objeto, asi puedo hacer mas.
+
+  crear objeto José de la plantilla Persona con edad : 19, peso : 42, nombre : "jose".
+
+  crear objeto Pedro de la plantilla Persona con edad : 15, peso : 45, nombre : "pedro".
+
+  Si te das cuenta la clase sirve como impresora de objetos
+
+  Debido a lo anterior se dice que un objeto es una instancia de una clase porque a partir de ella(clase) se crean objetos individuales.
+
+  Usando y creando objetos.
+
+  En javascript usamos new para decirle que no queremos una funcion normal, sino que queremos un objeto. Una vez le decimos eso, javascript nos permite usar la palabra reservada this.
+
+  usando la funcion como constructor:
+
+  function Persona (nombre, edad, peso) {
+      this.nombre = nombre;
+      this.edad = edad;
+      this.peso = peso;
+  }
+  objetos - instancias
+
+  var carlos = new Persona("carlos", 17, 42);
+  var jose =  new Persona("jose", 19, 42);
+  var pedro =  new Persona("pedro", 15, 45);
+
+  Una vez usado new js le "hace caso" a tus this y lo crea como propiedades del objeto que se esta creando, si no lo usas no les hará caso(1).
+
+  En conclusión, hay más teoría para this, sin embargo, es justo lo que necesites entender esta cuestion de los objetos. SI hay un error, que me digan. Espero que te haya servido.
+
+  * Si no usas new pero usas this, automáticamente las propiedades se aplicaran window
+
+  - Comentario hecho por Orlando en: https://www.udemy.com/course/javascript-de-cero-hasta-los-detalles/learn/lecture/4504258#questions/3918398
+
+  1.- ¿En Javascript cómo identificamos un método?
+
+  Un método es una función definida dentro de una clase o un algoritmo asociado a un objeto.Es básicamente una "acción" que un objeto puede llevar a acabo.
+
+  En javascript identificas un método dentro de un constructor o un objeto literal.Ejemplo:
+
+  Un constructor:
+
+  function Persona(Nombre, Apellido, Edad){
+      this.nombre=Nombre;
+      this.apellido=Apellido;
+      this.edad=Edad;
+      //esto es un método
+      this.obtenerPersona = function(){
+          return this.nombre + " " + this.apellido + " " + this.Edad;
+      }
+  }
+  Un objeto literal.
+
+  var persona =  {
+      comer : function(){
+          console.log("hola estoy comiendo");
+      }
+  }
+  persona.comer();
+
+  Diferencias:
+
+  La diferencia entre un método y las funciones es que, como había comentado antes un método es una función asociado o "ligado" a un objeto y mientras una función es un simple bloque de código independiente que puede ser llamado en un programa.
+
+  2.-Otra cosa mas, no puedo pasar por parámetro números.Al querer pasar la edad por parametro me sale undefined.
+
+  R.Si puedes pasar cualquier tipo de datos que requieras.Tu problema, es que comiste un error de tipeo es decir:
+
+  function Persona(Nombre, Apellido, Edad){
+      this.nombre=Nombre;
+      this.apellido=Apellido;
+      this.edad=Edad;
+      //esto es un método
+      this.obtenerPersona = function(){
+          return this.nombre + " " + this.apellido + " " + this.Edad;
+      }
+  }
+
+  en tu método imprimiste "this.Edad" mientras que tú propiedad es "edad" seria algo como this.edad.
+
+  function Persona(Nombre, Apellido, Edad){
+      this.nombre=Nombre;
+      this.apellido=Apellido;
+      this.edad=Edad;
+      //esto es un método
+      this.obtenerPersona = function(){
+          return this.nombre + " " + this.apellido + " " + this.edad;
+      }
+  }
+
+  PD: Quiero ampliar un poco el tema de las funciones vs metodos.
+
+  Primero imagina que yo empiezo con un programa simple, personas:
+
+  var contactos = [{nombre : "maria", telefono : 99989898},{nombre : "pedro", telefono : 9989787},{nombre : "jose", telefono : 99989898},{nombre : "maria", telefono : 99989898}];
+
+  //creo mis funciones
+  function obtenerNumero(obj){
+    console.log("el numero de "+ obj.nombre + " es "+ obj.telefono);
+  }
+
+  function llamar(obj){
+    console.log(obj.nombre + "llamando desde el numero " + obj.telefono);
+  }
+
+  //llamo la funcion y le paso el objeto
+  obtenerNumero(contactos[0]); // "el numero de maria es 99989898"
+  obtenerNumero(contactos[1]); // "el numero de pedro es 9989787"
+
+  llamar(contactos[0]); //"maria llamado desde el numero 99989898";
+
+  Todo OK. sin embargo podemos hacer mas legible y organizado el código si tomamos un enfoque Orientado a objetos.(Ojo que estamos usando objetos de manera literal) podríamos incluso haber asignado los métodos a cada uno de los objetos directamente pero podría llegar a ser algo ilegible.
+
+  contactos[0].llamar(); //"maria llamado desde el numero 99989898";
+  Ahora nosotros podriamos crear plantillas o "moldes" para los contactos.Es decir podemos crear entidades en base a un molde.
+
+  function Contacto(nombre, telefono){
+      this.nombre = nombre;
+      this.telefono = telefono;
+
+      //llamar ahora es un metodo
+      this.llamar = function(){
+          console.log(this.nombre + "esta llamado desde " + this.numero);
+      }
+  }
+
+  Ahora llamar() es un método por que está asociado a nuestra clase persona.Listo, ahora podemos crear varios objetos a creandolos gracias a nuestra clase/constructor.
+
+  var jose = new Contacto("jose", 9797878);
+  var maria= new Contacto("Maria", 34343343);
+  var pedro= new Contacto("Pedro", 33333);
+  Ahora podemos hacer uso de nuestros métodos.
+
+  pedro.llamar();// "pedro esta llamando desde 9797878"
+
+  Y asi con los demás objetos.
+
+*/
